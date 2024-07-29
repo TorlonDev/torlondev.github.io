c7de7c44f5d9b84d4c28719ee9064b74f74f0605
Torlon Dev <torlon.dev@gmail.com>
2024-07-29 15:15:52 +0700

    Update 03_CV.jsx

--- v2/components/03_CV.jsx
+++ v2/components/03_CV.jsx
@@ -1,6 +1,12 @@
 const langCV = {
-  cv_developing: { EN: 'New CV is developing.', TH: 'ซีวีเวอร์ชันใหม่กำลังอยู่ในระหว่างการพัฒนา' },
-  go_old_cv: { EN: 'Go to old version', TH: 'ไปที่ซีวีเวอร์ชันเก่า' },
+  cv_developing: { 
+    EN: 'New CV is developing.', TH: 'ซีวีเวอร์ชันใหม่กำลังอยู่ในระหว่างการพัฒนา',
+    CN: '新的简历正在开发中' 
+  },
+  go_old_cv: { 
+    EN: 'Go to old version', TH: 'ไปที่ซีวีเวอร์ชันเก่า',
+    CN: '转到旧版本' 
+  },
 }
 
 const CV = () => {
