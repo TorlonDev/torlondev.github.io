a5ffd7eed7771b0ca0f487d477a3e53bda05000d
Torlon Dev <torlon.dev@gmail.com>
2024-08-08 00:38:30 +0700

    Update Scroll

--- v2/components/02_Home.jsx
+++ v2/components/02_Home.jsx
@@ -1,11 +1,23 @@
 const Home = () => {
   const {
     lang, currentLanguage: l, isShowSideBar,
-    isSlideCover, setIsSlideCover,
   } = useContext(Context)
 
   const styleTextMobile = `clamp(20px, ${isShowSideBar ? '4vw' : '6vw'}, 36px)`
   const styleTextMobileFB = `clamp(16px, ${isShowSideBar ? '1vw' : '5vw'}, 42px)`
+
+  const scrollToCover = (idCover = 'snow_falling') => {
+    document.getElementById(idCover).scrollIntoView({
+      behavior: 'smooth', block: 'nearest', inline: 'center'
+    });
+  }
+
+  const ArrowChangeCover = ({ direction = 'right', idCoverTo = 'spring_falling', leftAbsolute = '90%' }) => {
+    return <div className="change_cover cover_content_center" style={{ left: leftAbsolute }}
+      onClick={() => { scrollToCover(idCoverTo) }}>
+      <span class={`fa-solid fa-arrow-${direction}`}></span>
+    </div>
+  }
 
   // componentDidMount
   useEffect(() => {
@@ -13,22 +25,6 @@ const Home = () => {
     spring_falling()
     raining()
     btc_falling()
-
-    if (!isSlideCover) {
-      setTimeout(() => {
-        document.getElementById('btc_falling').scrollIntoView({
-          behavior: 'smooth', block: 'nearest', inline: 'center'
-        });
-        setTimeout(() => {
-          document.getElementById('snow_falling').scrollIntoView({
-            behavior: 'smooth', block: 'nearest', inline: 'center'
-          });
-          setTimeout(() => {
-            setIsSlideCover(true)
-          }, 2000)
-        }, 1000)
-      }, 2000)
-    }
   }, []);
 
   return <>
@@ -38,73 +34,100 @@ const Home = () => {
           .cover_home {
             text-shadow: var(--BGColor) -3px 0px, var(--BGColor) 0px 3px, var(--BGColor) 3px 0px, var(--BGColor) 0px -3px;
           }
-          .raindrop {
-              position: absolute;
-              width: 2px;
-              height: 15px;
-              background: rgba(255, 255, 255, 0.9); /* Light color for the raindrops */
-              border-radius: 50%;
-              opacity: 0.9;
-              animation: fall linear infinite;
-          }
-          .btc {
-              position: absolute;
-              width: 28px;
-              height: 28px;
-              animation: btcfall linear infinite;
-          }
-          @keyframes fall {
-              from {
-                  transform: translateY(0); /* Start above the viewport */
-              }
-              to {
-                  transform: translateY(90vh); /* End below the viewport */
-              }
-          }
-          @keyframes btcfall {
-              from {
-                  transform: translateY(0) rotate(1deg); /* Start above the viewport */
-              }
-              to {
-                  transform: translateY(90vh) rotate(360deg); /* End below the viewport */
-              }
-          }
           .leaf {
             position: absolute;
-            width: 20px; /* Width of the leaf */
-            height: 20px; /* Height of the leaf */
+            width: 20px;
+            height: 20px;
             border-radius: 50% 50% 50% 0; /* Leaf shape */
             transform: rotate(-45deg); /* Rotate to make it more leaf-like */
-            opacity: 0.8; /* Slightly transparent */
+            opacity: 0.8;
             animation: springfall linear infinite;
           }
-
+          .raindrop {
+            position: absolute;
+            width: 2px;
+            height: 15px;
+            background: rgba(255, 255, 255, 0.9); /* Light color for the raindrops */
+            border-radius: 50%;
+            opacity: 0.9;
+            animation: fall linear infinite;
+          }
+          .btc {
+            position: absolute;
+            width: 28px;
+            height: 28px;
+            animation: btcfall linear infinite;
+          }
+          @keyframes fall {
+            from {
+                transform: translateY(0); /* Start above the viewport */
+            }
+            to {
+                transform: translateY(90vh); /* End below the viewport */
+            }
+          }
           @keyframes springfall {
-              from {
-                  transform: translate(0, 0) rotate(1deg) scale(0.2);
-              }
-              to {
-                  transform: translate(-150px, 90vh) rotate(360deg) scale(0.8);
-              }
+            from {
+                transform: translate(0, 0) rotate(1deg) scale(0.2);
+            }
+            to {
+                transform: translate(-150px, 90vh) rotate(360deg) scale(0.8);
+            }
+          }
+          @keyframes btcfall {
+            from {
+                transform: translateY(0) rotate(1deg);
+            }
+            to {
+                transform: translateY(90vh) rotate(360deg);
+            }
           }
 
+          .fa-arrow-left > path {
+            stroke: var(--BGColor);
+            stroke-width: 25px;
+          }
+
+          .fa-arrow-right > path {
+            stroke: var(--BGColor);
+            stroke-width: 25px;
+          }
+
+          .cover_content_center {
+            z-index: 99;
+            position: absolute; 
+            top: 50%;
+            transform: translate(-50%, -50%);
+            text-align: center;
+            align-content: center;
+          }
         ` + mobileCSS(`
           .cover_home {
             font-size: ${isFBApp ? styleTextMobileFB : styleTextMobile};
             border: 5px solid var(--color);
           }
           .raindrop {
-              width: 1px;
-              height: 6px;
+            width: 1px;
+            height: 6px;
           }
           .btc {
-              width: 18px;
-              height: 18px;
+            width: 18px;
+            height: 18px;
+          }
+          .change_cover {
+            width: 70px;
+            height: 70px;
+            cursor: pointer;
           }
         `) + desktopCSS(`
           .cover_home {
             font-size: clamp(36px, ${isFBApp ? '5vw' : '6vw'}, 60px);
             border: 10px solid var(--color);
+          }
+          .change_cover {
+            width: 100px;
+            height: 100px;
+            cursor: pointer;
           }
         `)
       }
@@ -112,7 +135,7 @@ const Home = () => {
     <div className="cover_home" style={{
       aspectRatio: '1000 / 550',
       position: 'relative',
-      overflow: 'hidden', overflowX: 'scroll',
+      overflow: 'hidden', overflowX: 'hidden',
       whiteSpace: 'nowrap'
     }}>
       <div id="snow_falling" style={{
@@ -122,11 +145,12 @@ const Home = () => {
         backgroundSize: '100% 100%',
       }}>
         <canvas id="canvas_snow_falling" style={{ width: '100%', height: '100%', }} />
-        {/* <div style={{ zIndex: '999',position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center' }}>
-          <span>{'Test'}</span><br /><br />
-          <span>{'Test'}</span><br /><br />
-          <span>{'Test'}</span>
-        </div> */}
+        <div className="cover_content_center" style={{ left: '50%' }}>
+          <span>{lang.hello_world[l].toUpperCase()}</span><br /><br />
+          <span>{lang.i_am_developer[l].toUpperCase()}</span><br /><br />
+          <span>{lang.i_am_cool[l].toUpperCase()}</span>
+        </div>
+        <ArrowChangeCover direction='right' idCoverTo='spring_falling' leftAbsolute = '90%'/>
       </div>
 
       <div id="spring_falling" style={{
@@ -135,11 +159,13 @@ const Home = () => {
         width: '100%', height: '100%',
         backgroundSize: '100% 100%',
       }}>
-        {/* <div style={{ zIndex: '999', position: 'absolute', top: '50%', left: '150%', transform: 'translate(-50%, -50%)', textAlign: 'center' }}>
-          <span>{'Test 2'}</span><br /><br />
-          <span>{'Test 2'}</span><br /><br />
-          <span>{'Test 2'}</span>
-        </div> */}
+        <ArrowChangeCover direction='left' idCoverTo='snow_falling' leftAbsolute = '110%'/>
+        <div className="cover_content_center" style={{ left: '150%' }}>
+          <span>{lang.hello_world[l].toUpperCase()}</span><br /><br />
+          <span>{lang.i_am_developer[l].toUpperCase()}</span><br /><br />
+          <span>{lang.i_am_single[l].toUpperCase()}</span>
+        </div>
+        <ArrowChangeCover direction='right' idCoverTo='rain_falling' leftAbsolute = '190%'/>
       </div>
 
       <div id="rain_falling" style={{
@@ -148,11 +174,13 @@ const Home = () => {
         width: '100%', height: '100%',
         backgroundSize: '100% 100%',
       }}>
-        {/* <div style={{ zIndex: '999',position: 'absolute', top: '50%', left: '250%', transform: 'translate(-50%, -50%)', textAlign: 'center' }}>
+        <ArrowChangeCover direction='left' idCoverTo='spring_falling' leftAbsolute = '210%'/>
+        <div className="cover_content_center" style={{ left: '250%' }}>
           <span>{lang.hello_world[l].toUpperCase()}</span><br /><br />
           <span>{lang.i_am_developer[l].toUpperCase()}</span><br /><br />
-          <span>{lang.i_like_coding[l].toUpperCase()}</span>
-        </div> */}
+          <span>{lang.i_love_coding[l].toUpperCase()}</span>
+        </div>
+        <ArrowChangeCover direction='right' idCoverTo='btc_falling' leftAbsolute = '290%'/>
       </div>
 
       <div id="btc_falling" style={{
@@ -161,13 +189,13 @@ const Home = () => {
         width: '100%', height: '100%',
         backgroundSize: '100% 100%',
       }}>
-        {/* <div style={{ zIndex: '999', position: 'absolute', top: '50%', left: '350%', transform: 'translate(-50%, -50%)', textAlign: 'center' }}>
-          <span>{'Test 3'}</span><br /><br />
-          <span>{'Test 3'}</span><br /><br />
-          <span>{'Test 3'}</span>
-        </div> */}
+        <ArrowChangeCover direction='left' idCoverTo='rain_falling' leftAbsolute = '310%'/>
+        <div className="cover_content_center" style={{ left: '350%' }}>
+          <span>{lang.hello_world[l].toUpperCase()}</span><br /><br />
+          <span>{lang.i_am_trader[l].toUpperCase()}</span><br /><br />
+          <span>{lang.i_love_trading[l].toUpperCase()}</span>
+        </div>
       </div>
-
       {/* <img src="v2/img/cover1.jpg"
         style={{ display: 'inline', width: '100%', height: '100%' }}
         loading="eager"
