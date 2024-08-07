dd7bcdd93dd63715e4ff5078337ec6c42e83aa3e
Torlon Dev <torlon.dev@gmail.com>
2024-08-07 17:59:19 +0700

    Update code

--- v2/components/02_Home.jsx
+++ v2/components/02_Home.jsx
@@ -6,21 +6,26 @@ const Home = () => {
 
   // componentDidMount
   useEffect(() => {
+    load_snow()
+    spring_falling()
     raining()
+    btc_falling()
+
+    document.getElementById('btc_falling').scrollIntoView({
+      behavior: 'smooth', block: 'nearest', inline: 'center'
+    });
+
+    setTimeout(()=> {
+      document.getElementById('snow_falling').scrollIntoView({
+        behavior: 'smooth', block: 'nearest', inline: 'center'
+      });
+    }, 1000)
   }, []);
 
   return <>
     <style>
       {
         ` 
-          /*.cover_home {
-            white-space: nowrap;
-            -ms-overflow-style: none; 
-            scrollbar-width: none;
-          }
-          .cover_home::-webkit-scrollbar {
-            display: none;
-          }*/
           .cover_home {
             text-shadow: var(--BGColor) -3px 0px, var(--BGColor) 0px 3px, var(--BGColor) 3px 0px, var(--BGColor) 0px -3px;
           }
@@ -28,10 +33,16 @@ const Home = () => {
               position: absolute;
               width: 2px;
               height: 15px;
-              background: rgba(255, 255, 255, 0.6); /* Light color for the raindrops */
+              background: rgba(255, 255, 255, 0.9); /* Light color for the raindrops */
               border-radius: 50%;
-              opacity: 0.7;
+              opacity: 0.9;
               animation: fall linear infinite;
+          }
+          .btc {
+              position: absolute;
+              width: 28px;
+              height: 28px;
+              animation: btcfall linear infinite;
           }
           @keyframes fall {
               from {
@@ -41,10 +52,45 @@ const Home = () => {
                   transform: translateY(90vh); /* End below the viewport */
               }
           }
+          @keyframes btcfall {
+              from {
+                  transform: translateY(0) rotate(1deg); /* Start above the viewport */
+              }
+              to {
+                  transform: translateY(90vh) rotate(360deg); /* End below the viewport */
+              }
+          }
+          .leaf {
+            position: absolute;
+            width: 20px; /* Width of the leaf */
+            height: 20px; /* Height of the leaf */
+            border-radius: 50% 50% 50% 0; /* Leaf shape */
+            transform: rotate(-45deg); /* Rotate to make it more leaf-like */
+            opacity: 0.8; /* Slightly transparent */
+            animation: springfall linear infinite;
+          }
+
+          @keyframes springfall {
+              from {
+                  transform: translate(0, 0) rotate(1deg) scale(0.2);
+              }
+              to {
+                  transform: translate(-150px, 90vh) rotate(360deg) scale(0.8);
+              }
+          }
+
         ` + mobileCSS(`
           .cover_home {
             font-size: ${isFBApp ? styleTextMobileFB : styleTextMobile};
             border: 5px solid var(--color);
+          }
+          .raindrop {
+              width: 1px;
+              height: 6px;
+          }
+          .btc {
+              width: 18px;
+              height: 18px;
           }
         `) + desktopCSS(`
           .cover_home {
@@ -60,84 +106,78 @@ const Home = () => {
       overflow: 'hidden', overflowX: 'scroll',
       whiteSpace: 'nowrap'
     }}>
+      <div id="snow_falling" style={{
+        backgroundImage: 'url(v2/img/cover2.jpg)',
+        display: 'inline-block',
+        width: '100%', height: '100%',
+        backgroundSize: '100% 100%',
+      }}>
+        <canvas id="canvas_snow_falling" style={{ width: '100%', height: '100%', }} />
+        {/* <div style={{ zIndex: '999',position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center' }}>
+          <span>{'Test'}</span><br /><br />
+          <span>{'Test'}</span><br /><br />
+          <span>{'Test'}</span>
+        </div> */}
+      </div>
+
+      <div id="spring_falling" style={{
+        backgroundImage: 'url(v2/img/cover3.jpg)',
+        display: 'inline-block',
+        width: '100%', height: '100%',
+        backgroundSize: '100% 100%',
+      }}>
+        {/* <div style={{ zIndex: '999', position: 'absolute', top: '50%', left: '150%', transform: 'translate(-50%, -50%)', textAlign: 'center' }}>
+          <span>{'Test 2'}</span><br /><br />
+          <span>{'Test 2'}</span><br /><br />
+          <span>{'Test 2'}</span>
+        </div> */}
+      </div>
+
       <div id="rain_falling" style={{
         backgroundImage: 'url(v2/img/cover1.jpg)',
         display: 'inline-block',
         width: '100%', height: '100%',
         backgroundSize: '100% 100%',
-      }}
-      ><div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center' }}>
+      }}>
+        {/* <div style={{ zIndex: '999',position: 'absolute', top: '50%', left: '250%', transform: 'translate(-50%, -50%)', textAlign: 'center' }}>
           <span>{lang.hello_world[l].toUpperCase()}</span><br /><br />
           <span>{lang.i_am_developer[l].toUpperCase()}</span><br /><br />
           <span>{lang.i_like_coding[l].toUpperCase()}</span>
-        </div>
+        </div> */}
       </div>
-      <div style={{
-        backgroundImage: 'url(v2/img/cover2.jpg)',
+
+      <div id="btc_falling" style={{
+        backgroundImage: 'url(v2/img/cover4.jpg)',
         display: 'inline-block',
         width: '100%', height: '100%',
         backgroundSize: '100% 100%',
-        textAlign: 'center',
       }}>
+        {/* <div style={{ zIndex: '999', position: 'absolute', top: '50%', left: '350%', transform: 'translate(-50%, -50%)', textAlign: 'center' }}>
+          <span>{'Test 3'}</span><br /><br />
+          <span>{'Test 3'}</span><br /><br />
+          <span>{'Test 3'}</span>
+        </div> */}
       </div>
 
-
-      {/* <div style={{ width: '100%', height: '100%', 
-        whiteSpace: 'nowrap', overflow: 'hidden', 
-        overflowX: 'hidden', overflowY: 'hidden' }}> */}
       {/* <img src="v2/img/cover1.jpg"
-          style={{ display: 'inline', width: '100%', height: '100%' }}
-          loading="eager"
-        />
-        <img src="v2/img/cover2.jpg"
-          style={{ display: 'inline', width: '100%', height: '100%' }}
-          loading="eager"
-        /> */}
-
-      {/* <div style={{
-          backgroundImage: 'url(v2/img/cover1.jpg)',
-          display: 'inline-block',
-          aspectRatio: '1000 / 550',
-          width: '100%', height: '100%',
-          backgroundSize: '100% 100%',
-          textAlign: 'center', 
-        }}> */}
-      {/* <div className="flex flex-col items-center"
-            style={{
-              justifyContent: 'space-around',
-              position: 'absolute', top: 0,
-              width: '100%', height: '90%',
-              backgroundColor: 'rgba(0,0,0,0.2)'
-            }}> */}
-      {/* <span>{lang.hello_world[l].toUpperCase()}</span><br/>
-            <span>{lang.i_am_developer[l].toUpperCase()}</span><br/>
-            <span>{lang.i_like_coding[l].toUpperCase()}</span> */}
-      {/* </div> */}
-      {/* <div id="rain_falling" className="animationFalling"
-            style={{
-              position: 'absolute', top: 0,
-              width: '100%', height: '90%',
-              backgroundColor: 'transparent'
-            }}
-          >
-          </div> */}
-      {/* </div> */}
-      {/* <div style={{
-          backgroundImage: 'url(v2/img/cover2.jpg)',
-          display: 'inline-block',
-          width: '100%', height: '100%',
-          backgroundSize: '100% 100%',
-        }}>
-          
-        </div> */}
-      {/* </div> */}
+        style={{ display: 'inline', width: '100%', height: '100%' }}
+        loading="eager"
+      />
+      <img src="v2/img/cover2.jpg"
+        style={{ display: 'inline', width: '100%', height: '100%' }}
+        loading="eager"
+      /> */}
     </div>
     <div style={{
       position: 'relative',
       width: '100%',
       height: '100vh',
-      backgroundColor: 'var(--BGColor)'
+      backgroundColor: 'var(--BGColor)',
+      color: 'var(--color)',
+      border: 'unset',
+      textShadow: 'unset'
     }}>
+      <span>{lang.coming_soon[l]}</span>
     </div>
   </>
 }
