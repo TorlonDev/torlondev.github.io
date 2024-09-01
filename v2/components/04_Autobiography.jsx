97a94a60888a0726c16e56d3a02f64a7c4de675b
Torlon Dev <torlon.dev@gmail.com>
2024-09-01 23:21:12 +0700

    Update 04_Autobiography.jsx

--- v2/components/04_Autobiography.jsx
+++ v2/components/04_Autobiography.jsx
@@ -30,11 +30,11 @@ const AutoBiography = () => {
             width="324" height={`${isFBApp ? '623px' : '553px'}`} style={{ overflow: 'hidden', pointerEvents: 'none', border: 'none' }} frameborder="0" ></iframe>
         </a>
       </div>
-      <div style={{ width: '324px', height: `${isFBApp ? '480px' : '430px'}` }}>
+      <div style={{ width: '324px', height: `${isFBApp ? '490px' : '430px'}` }}>
         <span className="text-xl">2016-2017</span>
         <a href="https://www.facebook.com/torn.onlyalone" target="_blank">
           <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto%2F%3Ffbid%3D378241239184959%26set%3Da.131442370531515&show_text=false&width=324"
-            width="324" height={`${isFBApp ? '480px' : '430px'}`} style={{ overflow: 'hidden', pointerEvents: 'none', border: 'none' }} frameborder="0" ></iframe>
+            width="324" height={`${isFBApp ? '490px' : '430px'}`} style={{ overflow: 'hidden', pointerEvents: 'none', border: 'none' }} frameborder="0" ></iframe>
         </a>
       </div>
       <div style={{ width: '324px', height: `${isFBApp ? '403px' : '373px'}` }}>
