4d7d5849a41d16b76ffed5cac65f51d1b5828396
Torlon Dev <torlon.dev@gmail.com>
2024-09-01 22:25:34 +0700

    Update 04_Autobiography.jsx

--- v2/components/04_Autobiography.jsx
+++ v2/components/04_Autobiography.jsx
@@ -22,8 +22,23 @@ const AutoBiography = () => {
       }
     `)}</style>
 
-    <div className="fb_wrapper" style={{ display: "flex", flexDirection: "row", gap: '10px', justifyContent: 'center', flexWrap: 'wrap', width: '100%'}}>
+    <div className="fb_wrapper" style={{ display: "flex", flexDirection: "row", gap: '10px', justifyContent: 'center', flexWrap: 'wrap', width: '100%', minHeight: '100vh' }}>
       <div style={{ width: '280px', height: `${isFBApp ? '623px' : '553px'}` }}>
+        <span className="text-xl">2010-2015</span>
+        <a href="https://www.facebook.com/tor.onlyalone" target="_blank">
+          <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto%2F%3Ffbid%3D692949137434104%26set%3Da.255540504508305&show_text=false&width=280"
+            width="280" height={`${isFBApp ? '623px' : '553px'}`} style={{ overflow: 'hidden', pointerEvents: 'none', border: 'none' }} frameborder="0" ></iframe>
+        </a>
+      </div>
+      <div style={{ width: '280px', height: `${isFBApp ? '470px' : '430px'}` }}>
+        <span className="text-xl">2016-2017</span>
+        <a href="https://www.facebook.com/torn.onlyalone" target="_blank">
+          <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto%2F%3Ffbid%3D378241239184959%26set%3Da.131442370531515&show_text=false&width=280"
+            width="280" height={`${isFBApp ? '470px' : '430px'}`} style={{ overflow: 'hidden', pointerEvents: 'none', border: 'none' }} frameborder="0" ></iframe>
+        </a>
+      </div>
+
+      {/* <div style={{ width: '280px', height: `${isFBApp ? '623px' : '553px'}` }}>
         <span className="text-xl">2010-2015</span>
         <a href="https://www.facebook.com/tor.onlyalone" target="_blank">
           <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto%2F%3Ffbid%3D692949137434104%26set%3Da.255540504508305&show_text=false&width=280"
@@ -44,8 +59,10 @@ const AutoBiography = () => {
           <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto%2F%3Ffbid%3D1359321627882023%26set%3Da.104674606680071&show_text=false&width=280"
             width="280" height={`${isFBApp ? '403px' : '373px'}`} style={{ overflow: 'hidden', pointerEvents: 'none', border: 'none' }} frameborder="0" ></iframe>
         </a>
-      </div>
+      </div> */}
+
+
     </div>
-    <br />< br/><br />< br/>
+    <br />< br /><br />< br />
   </div>
 }
