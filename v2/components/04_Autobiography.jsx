827db46404e45d5387d8cf4ada986db3700bee35
Torlon Dev <torlon.dev@gmail.com>
2024-09-04 00:42:11 +0700

    Update 04_Autobiography.jsx

--- v2/components/04_Autobiography.jsx
+++ v2/components/04_Autobiography.jsx
@@ -24,21 +24,21 @@ const AutoBiography = () => {
 
     <div className="fb_wrapper" style={{ display: "flex", flexDirection: "row", gap: '6px', justifyContent: 'center', flexWrap: 'wrap', width: '100%', minHeight: `${isFBApp ? '600px' : '520px'}` }}>
       <div style={{ width: '324px', height: `${isFBApp ? '600px' : '520px'}` }}>
-        <span className="text-xl">FB 2010-2015 (ชีวิตวัยเรียน)</span>
+        <span className="text-lg">FB 2010-2015 (ชีวิตวัยเรียน)</span>
         <a href="https://www.facebook.com/tor.onlyalone" target="_blank">
           <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto%2F%3Ffbid%3D692949137434104%26set%3Da.255540504508305&show_text=false&width=324"
             width="324" height={`${isFBApp ? '600px' : '520px'}`} style={{ overflow: 'hidden', pointerEvents: 'none', border: 'none' }} frameborder="0" ></iframe>
         </a>
       </div>
       <div style={{ width: '324px', height: `${isFBApp ? '500px' : '440px'}` }}>
-        <span className="text-xl">FB 2016-2017 (ชีวิตทำงานปีแรกหลังเรียนจบ)</span>
+        <span className="text-lg">FB 2016-2017 (ชีวิตทำงานปีแรกหลังเรียนจบ)</span>
         <a href="https://www.facebook.com/torn.onlyalone" target="_blank">
           <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto%2F%3Ffbid%3D378241239184959%26set%3Da.131442370531515&show_text=false&width=324"
             width="324" height={`${isFBApp ? '500px' : '440px'}`} style={{ overflow: 'hidden', pointerEvents: 'none', border: 'none' }} frameborder="0" ></iframe>
         </a>
       </div>
       <div style={{ width: '324px', height: `${isFBApp ? '403px' : '373px'}` }}>
-        <span className="text-xl">FB 2017-2023 (ชีวิตการทำงานก่อนอายุ 30)</span>
+        <span className="text-lg">FB 2017-2023 (ชีวิตการทำงานก่อนอายุ 30)</span>
         <a href="https://www.facebook.com/tron.json" target="_blank">
           <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto%2F%3Ffbid%3D1359321627882023%26set%3Da.104674606680071&show_text=false&width=324"
             width="324" height={`${isFBApp ? '403px' : '373px'}`} style={{ overflow: 'hidden', pointerEvents: 'none', border: 'none' }} frameborder="0" ></iframe>
