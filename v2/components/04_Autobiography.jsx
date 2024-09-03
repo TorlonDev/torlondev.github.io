54464f2d0e8f116c4476b800b2bafed9c6c6c78a
Torlon Dev <torlon.dev@gmail.com>
2024-09-03 23:42:32 +0700

    Update 04_Autobiography.jsx

--- v2/components/04_Autobiography.jsx
+++ v2/components/04_Autobiography.jsx
@@ -22,19 +22,19 @@ const AutoBiography = () => {
       }
     `)}</style>
 
-    <div className="fb_wrapper" style={{ display: "flex", flexDirection: "row", gap: '6px', justifyContent: 'center', flexWrap: 'wrap', width: '100%', minHeight: `${isFBApp ? '630px' : '550px'}` }}>
-      <div style={{ width: '324px', height: `${isFBApp ? '630px' : '553px'}` }}>
+    <div className="fb_wrapper" style={{ display: "flex", flexDirection: "row", gap: '6px', justifyContent: 'center', flexWrap: 'wrap', width: '100%', minHeight: `${isFBApp ? '620px' : '540px'}` }}>
+      <div style={{ width: '324px', height: `${isFBApp ? '620px' : '540px'}` }}>
         <span className="text-xl">FB 2010-2015 (ชีวิตวัยเรียน)</span>
         <a href="https://www.facebook.com/tor.onlyalone" target="_blank">
           <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto%2F%3Ffbid%3D692949137434104%26set%3Da.255540504508305&show_text=false&width=324"
-            width="324" height={`${isFBApp ? '630px' : '553px'}`} style={{ overflow: 'hidden', pointerEvents: 'none', border: 'none' }} frameborder="0" ></iframe>
+            width="324" height={`${isFBApp ? '620px' : '540px'}`} style={{ overflow: 'hidden', pointerEvents: 'none', border: 'none' }} frameborder="0" ></iframe>
         </a>
       </div>
-      <div style={{ width: '324px', height: `${isFBApp ? '490px' : '440px'}` }}>
+      <div style={{ width: '324px', height: `${isFBApp ? '500px' : '440px'}` }}>
         <span className="text-xl">FB 2016-2017 (ชีวิตทำงานปีแรกหลังเรียนจบ)</span>
         <a href="https://www.facebook.com/torn.onlyalone" target="_blank">
           <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto%2F%3Ffbid%3D378241239184959%26set%3Da.131442370531515&show_text=false&width=324"
-            width="324" height={`${isFBApp ? '490px' : '440px'}`} style={{ overflow: 'hidden', pointerEvents: 'none', border: 'none' }} frameborder="0" ></iframe>
+            width="324" height={`${isFBApp ? '500px' : '440px'}`} style={{ overflow: 'hidden', pointerEvents: 'none', border: 'none' }} frameborder="0" ></iframe>
         </a>
       </div>
       <div style={{ width: '324px', height: `${isFBApp ? '403px' : '373px'}` }}>
