f4d8ac03d881d419f653f82f9192aba5548509eb
Torlon Dev <torlon.dev@gmail.com>
2024-09-04 00:50:01 +0700

    Update 04_Autobiography.jsx

--- v2/components/04_Autobiography.jsx
+++ v2/components/04_Autobiography.jsx
@@ -22,12 +22,12 @@ const AutoBiography = () => {
       }
     `)}</style>
 
-    <div className="fb_wrapper" style={{ display: "flex", flexDirection: "row", gap: '6px', justifyContent: 'center', flexWrap: 'wrap', width: '100%', minHeight: `${isFBApp ? '600px' : '520px'}` }}>
-      <div style={{ width: '324px', height: `${isFBApp ? '600px' : '520px'}` }}>
+    <div className="fb_wrapper" style={{ display: "flex", flexDirection: "row", gap: '6px', justifyContent: 'center', flexWrap: 'wrap', width: '100%', minHeight: `${isFBApp ? '590px' : '510px'}` }}>
+      <div style={{ width: '324px', height: `${isFBApp ? '590px' : '510px'}` }}>
         <span className="text-lg">FB 2010-2015 (ชีวิตวัยเรียน)</span>
         <a href="https://www.facebook.com/tor.onlyalone" target="_blank">
           <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fphoto%2F%3Ffbid%3D692949137434104%26set%3Da.255540504508305&show_text=false&width=324"
-            width="324" height={`${isFBApp ? '600px' : '520px'}`} style={{ overflow: 'hidden', pointerEvents: 'none', border: 'none' }} frameborder="0" ></iframe>
+            width="324" height={`${isFBApp ? '590px' : '510px'}`} style={{ overflow: 'hidden', pointerEvents: 'none', border: 'none' }} frameborder="0" ></iframe>
         </a>
       </div>
       <div style={{ width: '324px', height: `${isFBApp ? '500px' : '440px'}` }}>
